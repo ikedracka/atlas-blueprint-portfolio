886fd586126ec24be17d2bbd8965bc3146b0d435
Ilona Kedracka <ilona.kedracka@gmail.com>
2023-05-22 14:04:17 +0200

    newrelic config

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script"
+import getAnrScript from '../utils/anr'
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <Script 
+            id="atlas-new-relic" 
+            dangerouslySetInnerHTML={{ __html: getAnrScript() }} 
+            strategy="beforeInteractive"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
